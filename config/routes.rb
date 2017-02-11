068f1f5f3357a2becd35e94a26cc01a90f1ea32e
Ehbraheem <ehbraheem@gmail.com>
2017-02-11 11:06:35 -0800

    re-added ui as root URL and merged AP+external

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
 
   get "/client-assets/:name.:format", :to => redirect("/client/client-assets/%{name}.%{format}")
 
-  get "/", to: redirect("/client/index.html")
+  # get "/", to: redirect("/client/index.html")
 
-  # get '/ui' => 'ui#index'
-  # get '/ui#' => 'ui#index'
+  get '/ui' => 'ui#index'
+  get '/ui#' => 'ui#index'
   #
-  # root "ui#index"
+  root "ui#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
