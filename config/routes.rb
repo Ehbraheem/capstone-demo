bc2d6a8d5411afafdd975544b5a7395cfa0467a6
Ehbraheem <ehbraheem@gmail.com>
2017-02-11 10:44:49 -0800

    added redirect

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
 
   get "/client-assets/:name.:format", :to => redirect("/client/client-assets/%{name}.%{format}")
 
-  get '/ui' => 'ui#index'
-  get '/ui#' => 'ui#index'
+  get "/", to: redirect("/client/index.html")
 
-  root "ui#index"
+  # get '/ui' => 'ui#index'
+  # get '/ui#' => 'ui#index'
+  #
+  # root "ui#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
