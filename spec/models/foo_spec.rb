e9cbb110565c0ffb14c4ca3c6960bd4b9595c433
Ehbraheem <ehbraheem@gmail.com>
2017-02-14 20:00:29 -0800

    added model specs and database cleaner for AR

---
+++ spec/models/foo_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe Foo, type: :model do
+
+  # add db cleanup
+  # include_context 'db_cleanup'
+  # include_context 'db_scope'
+  %w( db_cleanup db_scope).each { |action| include_context action}
+
+  context "created Foo (let)" do
+
+    let(:foo) { Foo.create(:name=> "test") }
+
+		it { expect(foo).to be_persisted }
+
+		it { expect(foo.name).to eq("test") }
+
+		it { expect(Foo.find(foo.id)).to_not be_nil }
+
+  end
+
+	context "created Foo (subject)" do
+
+		subject { Foo.create(:name=> "test") }
+
+		it { is_expected.to be_persisted }
+
+		it { expect(subject.name).to eq("test") }
+
+		it { expect(Foo.find(subject.id)).to_not be_nil }
+
+  end
+
+  context "created Foo (lazy)" do
+    let!(:before_count) { Foo.count } # eager instatiation
+    let(:foo) { Foo.create(:name=> "test") }
+
+    it { expect(foo).to be_persisted }
+
+    it { expect(foo.name).to eq("test") }
+
+    it { expect(Foo.find(foo.id)).to_not be_nil }
+
+    it { foo; expect(Foo.count).to eq(before_count + 1) } # called the function created by let to insert record in DB
+
+  end
+
+
+	# context "valid foo" do
+	# 	it "has a name" do
+	# 		foo = Foo.create(:name=> "test")
+	# 		expect(foo).to be_valid
+	# 		expect(foo.name).to_not be_nil
+	# 	end
+	# end
+end
