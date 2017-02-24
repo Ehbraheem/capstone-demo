bf0aff424b9664b78ccf4393ea5910b51eabd563
Ehbraheem <ehbraheem@gmail.com>
2017-02-24 03:05:39 -0800

    Add ThingImage as relationship for Imgae and Thing

--- spec/factories/things.rb
+++ spec/factories/things.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :thing do
     name         { Faker::Commerce.product_name }
     sequence(:description) { |n| n%5 === 0 ? nil : Faker::Lorem.paragraphs.join }
-    sequence(:notes) { |n| n%5 < 2 === 0 ? nil :  Faker::Lorem.paragraphs.join }
+    sequence(:notes) { |n| n%5 < 2 ? nil : Faker::Lorem.paragraphs.join }
 
     trait :with_description do
       description { Faker::Lorem.paragraphs.join }
@@ -15,6 +15,15 @@ FactoryGirl.define do
     trait :with_fields do
       :with_notes
       :with_description
+    end
+
+    trait :with_image do
+      transient do
+        image_count 1
+      end
+      after(:build) do |thing, props|
+        thing.thing_images << build_list(:thing_image, props.image_count, :thing=>thing)
+      end
     end
   end
 end
