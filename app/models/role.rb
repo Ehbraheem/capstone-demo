778d24c4c3bc3e8afb91e524721d2db15be58c65
Ehbraheem <ehbraheem@gmail.com>
2017-03-25 10:31:51 -0700

    Add Roles scope for relevant User roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,4 +5,9 @@ class Role < ActiveRecord::Base
   MEMBER="member"
 
   belongs_to :user, inverse_of: :roles
+
+  scope :relevant, ->(model_name, model_id) {
+    where("mname is null or (mname=:mname and (mid is null or mid=:mid))",
+      :mname=>model_name, :mid=>model_id)
+  }
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "user assigned roles" do
+    # we start with an instance of a Devise user in the database
+    let(:user) { FactoryGirl.create(:user) }
+
+    it "has roles" do
+      # we add two Role definition to the Devise user
+      user.roles.create(:role_name=>Role::ADMIN)
+      user.roles.create(:role_name=>Role::ORIGINATOR, :mname=>"Foo")
+      user.roles.create(:role_name=>Role::ORGANIZER, :mname=>"Bar", :mid=>1)
+      user.roles.create(:role_name=>Role::MEMBER, :mname=>"Baz", :mid=>1)
+
+      db_user = User.find(user.id)
+      expect(db_user.has_role([Role::ADMIN])).to be true
+      expect(db_user.has_role([Role::ORIGINATOR], "Bar")).to be false
+      expect(db_user.has_role([Role::ORIGINATOR], "Foo")).to be true
+      expect(db_user.has_role([Role::MEMBER], "Baz", 1)).to be true
+    end
+  end
 end
